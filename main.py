b198b5b7509f5e6c033fc7f3ffc1caab3d515012
Toh Zi Jie <toh.zijie@dhs.sg>
2013-01-28 20:56:13 +0800

    created itemdetail.html

--- main.py
+++ main.py
@@ -115,37 +115,37 @@ class Post_Item_Confirmed(webapp2.RequestHandler):
         creation_date = str((datetime.datetime.now() + datetime.timedelta(hours=8)).strftime("%d %B %Y %I:%M %p"))
         key_date = str(datetime.datetime.now() + datetime.timedelta(hours=8))
         Items(key_name = key_date, Title = title, Description = description, Price = price, Seller = seller, Seller_Name = User.get_by_key_name(user.email()).Name, Creation_Date = creation_date, Key_Date = key_date).put()
-        #mail
-        user_address = user.email()
-        sender_address = "DHShardcode <hardcodedhs@gmail.com>"
-        subject = "[DHS HARDCODE]Your item has been created."
-        body = ''' Your item has been created.
-    Title: %s
-    Description: %s
-    Price: %s
+        try:#mail
+            user_address = user.email()
+            sender_address = "DHShardcode <hardcodedhs@gmail.com>"
+            subject = "[DHS HARDCODE]Your item has been created."
+            body = ''' Your item has been created.
+        Title: %s
+        Description: %s
+        Price: %s
 
-    Thank you for using our service.
-                    '''%(title, description, price)
-        mail.send_mail(sender_address, user_address, subject, body)
-        self.redirect("/browse")
+        Thank you for using our service.
+                        '''%(title, description, price)
+            mail.send_mail(sender_address, user_address, subject, body)
+            self.redirect("/browse")
+        except:
+            self.redirect("/browse")
 
 class Item_Detail(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         key_name = self.request.get("key_name")
-        self.response.write("""Title: """ + Items.get_by_key_name(key_name).Title)
-        self.response.write("""<br>Description: """ + Items.get_by_key_name(key_name).Description)
-        self.response.write("""<br>Price: """ + Items.get_by_key_name(key_name).Price)
-        self.response.write("""<br>Seller: """ + Items.get_by_key_name(key_name).Seller.nickname())
-        self.response.write("""<br>Creation Date: """ + Items.get_by_key_name(key_name).Creation_Date)
-        for comment in Items.get_by_key_name(key_name).Comments:
-            self.response.write("""<br>""" + comment)
-        if user:
-            self.response.write("""<br><form name="comment_post" action="/item_detail" method="post">
-                                        <input type="text" name="key_name" style="display:none" value="%s" />
-                                        <input type="text" name="comment" multiline="true" /><br>
-                                        <button type="submit">Post Reply</button>
-                                        </form>""" % key_name)
+
+        template_values = {
+		'user':user,
+                'key_name':key_name,
+                'Items':Items,
+	}
+
+        template = jinja_environment.get_template('itemdetail.html')
+        self.response.out.write(template.render(template_values))
+
+            
     def post(self):
         user = users.get_current_user()
         key_name = self.request.get("key_name")
@@ -155,20 +155,34 @@ class Item_Detail(webapp2.RequestHandler):
         Items(key_name = key_name, Title = Items.get_by_key_name(key_name).Title, Description = Items.get_by_key_name(key_name).Description, \
               Price = Items.get_by_key_name(key_name).Price, Creation_Date = Items.get_by_key_name(key_name).Creation_Date, \
               Key_Date = Items.get_by_key_name(key_name).Key_Date, Seller = Items.get_by_key_name(key_name).Seller, Comments = comments_list).put()
-        user_address = Items.get_by_key_name(key_name).Seller.email()
-        sender_address = "DHShardcode <hardcodedhs@gmail.com>"
-        subject = "[DHS HARDCODE] %s commented on your item" % user.email()
-        body = '''%s commented on your item: %s
-Please visit dhshardcode.appspot.com to view your item.
-Thank you for using our service.''' %(user.email(), comment)
-        mail.send_mail(sender_address, user_address, subject, body)
-        self.response.write("""Title: """ + Items.get_by_key_name(key_name).Title)
-        self.response.write("""<br>Description: """ + Items.get_by_key_name(key_name).Description)
-        self.response.write("""<br>Price: """ + Items.get_by_key_name(key_name).Price)
-        self.response.write("""<br>Seller: """ + Items.get_by_key_name(key_name).Seller.nickname())
-        self.response.write("""<br>Creation Date: """ + Items.get_by_key_name(key_name).Creation_Date)
-        for comment in Items.get_by_key_name(key_name).Comments:
-            self.response.write("""<br>""" + comment)
+
+        try:
+            user_address = Items.get_by_key_name(key_name).Seller.email()
+            sender_address = "DHShardcode <hardcodedhs@gmail.com>"
+            subject = "[DHS HARDCODE] %s commented on your item" % user.email()
+            body = '''%s commented on your item: %s
+    Please visit dhshardcode.appspot.com to view your item.
+    Thank you for using our service.''' %(user.email(), comment)
+            mail.send_mail(sender_address, user_address, subject, body)
+
+
+            
+            self.response.write("""Title: """ + Items.get_by_key_name(key_name).Title)
+            self.response.write("""<br>Description: """ + Items.get_by_key_name(key_name).Description)
+            self.response.write("""<br>Price: """ + Items.get_by_key_name(key_name).Price)
+            self.response.write("""<br>Seller: """ + Items.get_by_key_name(key_name).Seller.nickname())
+            self.response.write("""<br>Creation Date: """ + Items.get_by_key_name(key_name).Creation_Date)
+            for comment in Items.get_by_key_name(key_name).Comments:
+                self.response.write("""<br>""" + comment)
+        except:
+            self.response.write("""Title: """ + Items.get_by_key_name(key_name).Title)
+            self.response.write("""<br>Description: """ + Items.get_by_key_name(key_name).Description)
+            self.response.write("""<br>Price: """ + Items.get_by_key_name(key_name).Price)
+            self.response.write("""<br>Seller: """ + Items.get_by_key_name(key_name).Seller.nickname())
+            self.response.write("""<br>Creation Date: """ + Items.get_by_key_name(key_name).Creation_Date)
+            for comment in Items.get_by_key_name(key_name).Comments:
+                self.response.write("""<br>""" + comment)
+        
 
         if user:
             
