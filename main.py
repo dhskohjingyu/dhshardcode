eeedc59f6f6dd9fd559ea10c110083215e64bb69
Toh Zi Jie <toh.zijie@dhs.sg>
2013-02-19 17:45:25 +0800

    admin.html

--- main.py
+++ main.py
@@ -368,50 +368,20 @@ class Activation(webapp2.RequestHandler):
 class Admin(webapp2.RequestHandler):
     def get(self):
         user=users.get_current_user()
-        if user and users.is_current_user_admin():
-            self.response.out.write('''
-                <table>
-                    <tr>
-                        <td>user</td>
-                        <td>delete user</td>
-                        <td>item</td>
-                        
-                    </tr>''')
-            for person in User.all():
-                self.response.out.write('''
-                    <tr>
-                        <form method='post' action='/deleteprofile'>
-                            <td><input type='text' name='user_email' value='%s'></td>
-                            <input type='text' name='redirect' value='/admin' style='display:none'>
-
-                            <td><button type='submit'>Delete his profile</button></td>
-                        </form>
-                        <td>
-                            <table>
-                                <tr>
-                                    <td>Item Name</td>
-                                    <td>Delete Item</td>
-                                </tr>
-                        '''%(person.Email))
-                for itemid in person.Sell_Items:
-
-                    print itemid
-                    item=Items.get_by_key_name(itemid)
-                    self.response.out.write('''
-                        <tr>
-                            <td>%s</td>
-                            <td>
-                                <form method='post' action='/item_delete'>
-                                    <input type='text' name='key_name' value='%s' style='display:none'>
-                                    <input type='text' name='redirect' value='/admin' style='display:none'>
-                                    <input type='text' name='user_email' value='%s' style='display:none'>
-                                    <button type='submit'>Delete item</button>
-                                </form>
-                            </td>
-                        </tr>'''%(item.Title,item.Key_Date,user.email()))
-                
-                self.response.out.write(''' </table></td>''')   
-                
+        
+        template_values = {
+		'user':user,
+                'users':users,
+                'User':User,
+                'Items':Items,
+                #'key_name':key_name,
+                #'Items':Items,
+                #'not_seller':not_seller,
+               # 'not_buyer':not_buyer,
+	}
+ 
+        template = jinja_environment.get_template('admin.html')
+        self.response.out.write(template.render(template_values))
                         
 class Expired(webapp2.RequestHandler):
     def get(self):
